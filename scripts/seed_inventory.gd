6ff0464705c9331ed3c1c05d6c2af61941f9c6b6
LeviathanLevi <leutholdlevi@gmail.com>
2023-02-19 15:43:38 -0800

    pushing more seed inventory and picker menu shid

--- scripts/seed_inventory.gd
+++ scripts/seed_inventory.gd
@@ -3,41 +3,59 @@ extends Node
 enum {WINTER, SPRING, SUMMER, FALL}
 
 var FoodPlant = {}
-	
 var PartPlant = {}
-	
-#part plants:
+
+#part plants types:
 var gearPlant
 var pistonPlant
 var bearingPlant
 var fuelPlant
 var springPlant
 
-#food plants:
+#food plants types:
 var potatoPlant
 var tomatoPlant
 var wheatPlant
 var carrotPlant
 var broccoliPlant
 
+#Inventory:
+var partSeedInventory = {}
+var foodSeedInventory = {}
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	partSeedInventory = {"gearSeeds": 0, "pistonSeeds": 0, "bearingSeeds": 0, "fuelSeeds": 0, "springSeeds": 0}
+	foodSeedInventory = {"potatoSeeds": 0, "tomatoSeeds": 0, "wheatSeeds": 0, "carrotSeeds": 0, "broccoliSeeds": 0}
+	
 	#Setup part plants
-	gearPlant = {"yieldMin": 10, "yieldMax": 25}
-	pistonPlant = {"yieldMin": 5, "yieldMax": 15}
-	bearingPlant = {"yieldMin": 5, "yieldMax": 10}
-	fuelPlant = {"yieldMin": 5, "yieldMax": 15}
-	springPlant = {"yieldMin": 1, "yieldMax": 2}
+	gearPlant = {"ID": 1, "yieldMin": 10, "yieldMax": 25}
+	pistonPlant = {"ID": 2, "yieldMin": 5, "yieldMax": 15}
+	bearingPlant = {"ID": 3, "yieldMin": 5, "yieldMax": 10}
+	fuelPlant = {"ID": 4, "yieldMin": 5, "yieldMax": 15}
+	springPlant = {"ID": 5, "yieldMin": 1, "yieldMax": 2}
 	
 	#setup food plants
-	potatoPlant = {"mature_time": 30, "preferred_season": "SUMMER", "death_season": "WINTER", "food_restored": 60 }
-	tomatoPlant = {"mature_time": 45, "preferred_season": "SPRING", "death_season": "FALL", "food_restored": 90 }
-	wheatPlant = {"mature_time": 20, "preferred_season": "SUMMER", "death_season": "SPRING", "food_restored": 20 }
-	carrotPlant = {"mature_time": 60, "preferred_season": "SPRING", "death_season": "WINTER", "food_restored": 100 }
-	broccoliPlant = {"mature_time": 25, "preferred_season": "FALL", "death_season": "SUMMER", "food_restored": 75 }
+	potatoPlant = {"ID": 1, "mature_time": 30, "preferred_season": "SUMMER", "death_season": "WINTER", "food_restored": 60 }
+	tomatoPlant = {"ID": 2, "mature_time": 45, "preferred_season": "SPRING", "death_season": "FALL", "food_restored": 90 }
+	wheatPlant = {"ID": 3, "mature_time": 20, "preferred_season": "SUMMER", "death_season": "SPRING", "food_restored": 20 }
+	carrotPlant = {"ID": 4, "mature_time": 60, "preferred_season": "SPRING", "death_season": "WINTER", "food_restored": 100 }
+	broccoliPlant = {"ID": 5, "mature_time": 25, "preferred_season": "FALL", "death_season": "SUMMER", "food_restored": 75 }
 	
 #**********************************************************************************************************************
 
+func get_part_seed_inventory() -> Dictionary:
+	return partSeedInventory
+	
+func get_food_seed_inventory() -> Dictionary:
+	return foodSeedInventory
+	
+func set_food_seed_inventory(foodInventory):
+	foodSeedInventory = foodInventory
+	
+func set_part_seed_inventory(partInventory):
+	partSeedInventory = partInventory
 
 func get_part_plant(index) -> Dictionary:
 	if index == 1:
@@ -68,3 +86,65 @@ func get_food_plant(index) -> Dictionary:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+#Seed getters
+func get_gear_seeds() -> int:
+	return partSeedInventory["gearSeeds"]
+	
+func get_piston_seeds() -> int:
+	return partSeedInventory["pistonSeeds"]
+	
+func get_bearing_seeds() -> int:
+	return partSeedInventory["bearingSeeds"]
+	
+func get_fuel_seeds() -> int:
+	return partSeedInventory["fuelSeeds"]
+
+func get_spring_seeds() -> int:
+	return partSeedInventory["springSeeds"]
+	
+func get_potato_seeds() -> int:
+	return partSeedInventory["potatoSeeds"]
+	
+func get_tomato_seeds() -> int:
+	return partSeedInventory["tomatoSeeds"]
+	
+func get_wheat_seeds() -> int:
+	return partSeedInventory["wheatSeeds"]
+	
+func get_carrot_seeds() -> int:
+	return partSeedInventory["carrotSeeds"]
+
+func get_broccoli_seeds() -> int:
+	return partSeedInventory["broccoliSeeds"]
+	
+#Seed setters
+func add_gear_seeds(seedCount):
+	partSeedInventory["gearSeeds"] = partSeedInventory["gearSeeds"] + seedCount
+	
+func add_piston_seeds(seedCount):
+	partSeedInventory["pistonSeeds"] = partSeedInventory["pistonSeeds"] + seedCount
+	
+func add_bearing_seeds(seedCount):
+	partSeedInventory["bearingSeeds"] = partSeedInventory["bearingSeeds"] + seedCount
+	
+func add_fuel_seeds(seedCount):
+	partSeedInventory["fuelSeeds"] = partSeedInventory["fuelSeeds"] + seedCount
+
+func add_spring_seeds(seedCount):
+	partSeedInventory["springSeeds"] = partSeedInventory["springSeeds"] + seedCount
+	
+func add_potato_seeds(seedCount):
+	partSeedInventory["potatoSeeds"] = partSeedInventory["potatoSeeds"] + seedCount
+	
+func add_tomato_seeds(seedCount):
+	partSeedInventory["tomatoSeeds"] = partSeedInventory["tomatoSeeds"] + seedCount
+	
+func add_wheat_seeds(seedCount):
+	partSeedInventory["wheatSeeds"] = partSeedInventory["wheatSeeds"] + seedCount
+	
+func add_carrot_seeds(seedCount):
+	partSeedInventory["carrotSeeds"] = partSeedInventory["carrotSeeds"] + seedCount
+
+func add_broccoli_seeds(seedCount):
+	partSeedInventory["broccoliSeeds"] = partSeedInventory["broccoliSeeds"] + seedCount
